7cf6830533dd316f315ef614a4f8e0a6aaf9ebd3
Rikki Lissaman <rikki.lissaman@outlook.com>
2023-07-28 11:56:06 -0400

    Update bhamm-pls-regression-analysis.R
    Add RT analysis.

--- analyses/bhamm-pls-regression-analysis.R
+++ analyses/bhamm-pls-regression-analysis.R
@@ -1,6 +1,6 @@
 # Name: bhamm-pls-regression-analysis.R
 # Author: Rikki Lissaman
-# Last Updated: July 18, 2023
+# Last Updated: July 28, 2023
 #
 # Description: This R script contains the code used to analyze data for "The 
 # interactive influence of chronological age and menopause status on the functional
@@ -9,20 +9,20 @@
 # 
 # Inputs: 
 # (1) 1 SPSS file (.sav) containing relevant data from the Brain Health at Midlife 
-# and Menopause (BHAMM) study (BHAM_N72_MA_31Pre41Post_MRIsample_CorrectedFA_RT.sav)
+# and Menopause (BHAMM) study (BHAM_N72_MA_31Pre41Post_MRIsample_CorrectedFA_RT.sav).
 #
 # Outputs:
 # N/A
 #
 # Additional Information:
-# - Run using R version 4.1.3 (2022-03-10) on MacBook Pro.
+# - Run using R version 4.1.3 (2022-03-10) on 2021 MacBook Pro (M1 chip).
 # - The plot generated below was amended outside of R (using Inkscape) to improve
-#   readability. 
+#   readability for publication. 
 
 
 # LOAD PACKAGES -----------------------------------------------------------
 
-# Load packages for data wrangling/analysis
+# Load packages for data wrangling/analysis.
 library(haven) # version 2.5.0
 library(janitor) # version 2.1.0
 library(tidyr) # version 1.2.0
@@ -46,45 +46,94 @@ bhamm <- bhamm %>%
   mutate(s2_meno_group = case_when(s2_meno_group == "Premenopause" ~ -1,
                                    s2_meno_group == "Postmenopause" ~ 1))
 
-# Create a new tibble, X, that contains the independent variables (a.k.a., the 
-# predictor variables). In this case, the independent variables are menopause 
-# status at session 2 (`s2_meno_group`) and age (years) at session 2 (`s2_age`). 
-X <- bhamm %>% select(s2_meno_group, s2_age)
+
+# RUN PLS REGRESSION FOR ACCURACY DATA ------------------------------------
+
+# Create a new tibble, x_acc, that contains the independent variables (a.k.a., the 
+# predictor variables) for the accuracy analysis. In this case, the independent 
+# variables are menopause status at session 2 (`s2_meno_group`) and age (years) 
+# at session 2 (`s2_age`). 
+x_acc <- bhamm %>% select(s2_meno_group, s2_age)
 
 # Standardize (i.e., z-score) age. Ensure it is stored as a numeric variable.
-X <- X %>% 
+x_acc <- x_acc %>% 
   mutate(s2_age = scale(s2_age, center = TRUE, scale = TRUE)) %>% 
   mutate(s2_age = as.numeric(s2_age))
 
-# Create a new tibble, Y, that contains the dependent variables (a.k.a., the outcome 
-# variables). In this case, the dependent variables are correct source rate - easy 
-# (`cs_rate_easy`), correct source rate - hard (`cs_rate_hard`), recognition rate 
-# - easy (`recog_rate_easy`), and recognition rate - hard (`recog_rate_hard`). 
-Y <- bhamm %>% select(cs_rate_easy, cs_rate_hard, recog_rate_easy, recog_rate_hard)
+# Create a new tibble, y_acc, that contains the dependent variables (a.k.a., the outcome 
+# variables) for the accuracy analysis. In this case, the dependent variables are 
+# correct source rate - easy (`cs_rate_easy`), correct source rate - hard (`cs_rate_hard`), 
+# recognition rate - easy (`recog_rate_easy`), and recognition rate - hard (`recog_rate_hard`). 
+y_acc <- bhamm %>% select(cs_rate_easy, cs_rate_hard, recog_rate_easy, recog_rate_hard)
 
 # Standardize (i.e., z-score) all of the dependent variables. Ensure all are stored 
 # as numeric variables.
-Y <- Y %>% 
+y_acc <- y_acc %>% 
   mutate_if(is.numeric, ~scale(., center = TRUE, scale = TRUE)) %>% 
   mutate_if(is.numeric, ~as.numeric(.))
 
+# Run PLS regression analysis on the accuracy data using `plsreg2`. This performs 
+# a PLS2 analysis; essentially, a PLS regression in the multivariate (or multi-response) case.
+set.seed(123)
+plsreg2_acc <- plsreg2(predictors = x_acc, responses = y_acc, comps = NULL, crosval = TRUE) 
 
-# RUN PLS REGRESSION ------------------------------------------------------
+# Examine the Q2 values for the LVs (a.k.a., components). Values >= 0.0975 are 
+# usually considered "significant".
+plsreg2_acc$Q2
 
-# Run a PLS regression analysis using `plsreg2`. This performs a PLS2 analysis; 
-# essentially, a PLS regression in the multivariate (or multi-response) case.
-plsreg2_model <- plsreg2(predictors = X, responses = Y, comps = 2, crosval = TRUE) # comps = PLS components (default = 2)
+# Examine the percentage of variance in X (age, menopause) and Y (performance measures) 
+# explained by the LVs.
+plsreg2_acc$expvar
 
-# First and foremost, examine the percentage of variance in X (age, menopause) 
-# and Y (performance measures) explained by the two components.
-round(plsreg2_model$expvar, 4)
+# Generate a correlation circle  plot for the model (similar to PCA).
+plot(plsreg2_acc, what = "variables", show.names = TRUE, xlab = "LV1", 
+     ylab = "LV2", main = NULL, cex = 1, pos = 4)
 
-# Generate a loading plot for the model (similar to PCA).
-plot(plsreg2_model, what = "variables", show.names = TRUE, xlab = "First Component", 
-     ylab = "Second Component", main = NULL, cex = 1, pos = 4)
 
-# Print the loadings of the predictor variables
-plsreg2_model$x.loads
+# RUN PLS REGRESSION FOR RT DATA ------------------------------------------
 
-# Print the loadings of the outcome variables.
-plsreg2_model$y.loads
+# Create a new tibble, x_rt, that contains the independent variables (a.k.a., the 
+# predictor variables) for the RT analysis. In this case, the independent 
+# variables are menopause status at session 2 (`s2_meno_group`) and age (years) 
+# at session 2 (`s2_age`). 
+x_rt <- bhamm %>% select(s2_meno_group, s2_age)
+
+# Standardize (i.e., z-score) age. Ensure it is stored as a numeric variable.
+x_rt <- x_rt %>% 
+  mutate(s2_age = scale(s2_age, center = TRUE, scale = TRUE)) %>% 
+  mutate(s2_age = as.numeric(s2_age))
+
+# Create a new tibble, y_rt, that contains the dependent variables (a.k.a., the outcome 
+# variables) for the RT analysis. In this case, the dependent variables are 
+# correct source RT - easy (`rt_cs_easy`), correct source RT - hard (`rt_cs_hard`), 
+# recognition RT - easy (`rt_recog_easy`), and recognition RT - hard (`rt_recog_hard`). 
+y_rt <- bhamm %>% select(rt_cs_easy, rt_cs_hard, rt_recog_easy, rt_recog_hard)
+
+# Standardize (i.e., z-score) all of the dependent variables. Ensure all are stored 
+# as numeric variables.
+y_rt <- y_rt %>% 
+  mutate_if(is.numeric, ~scale(., center = TRUE, scale = TRUE)) %>% 
+  mutate_if(is.numeric, ~as.numeric(.))
+
+# Identify and remove two participants (issue raised separately) who provided no 
+# correct recognition judgments for the easy task. These participants do not, by 
+# definition, have RT data for this condition, and `plsreg2` does not allow missing data.
+which(is.na(y_rt$rt_recog_easy)) # 10, 44
+x_rt <- x_rt[-c(10, 44), ]
+y_rt <- y_rt[-c(10, 44), ]
+
+# Run PLS regression analysis on the RT data using `plsreg2`.
+set.seed(123) 
+plsreg2_rt <- plsreg2(predictors = x_rt, responses = y_rt, comps = NULL, crosval = TRUE) 
+
+# Examine the Q2 values for the LVs (a.k.a., components). Values >= 0.0975 are 
+# usually considered "significant".
+plsreg2_rt$Q2
+
+# Examine the percentage of variance in X (age, menopause) and Y (performance measures) 
+# explained by the LVs.
+plsreg2_rt$expvar
+
+# Generate a correlation circle  plot for the model (similar to PCA).
+plot(plsreg2_rt, what = "variables", show.names = TRUE, xlab = "LV1", 
+     ylab = "LV2", main = NULL, cex = 1, pos = 4)
